9a89450524b882bbed5e041c7f90286089dcbb19
bsottek <bsottek@gmail.com>
2021-07-25 15:17:25 -0500

    Categories get all route

--- models/index.js
+++ models/index.js
@@ -7,7 +7,7 @@ const ProductTag = require('./ProductTag');
 // Products belongsTo Category
 Product.belongsTo(Category, { 
   foreignKey: 'category_id'
-})
+});
 
 // Categories have many Products
 Category.hasMany(Product, {
@@ -17,15 +17,13 @@ Category.hasMany(Product, {
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
   through: ProductTag,
-  as: tag_id,
-  foreignKey: product_id
-})
+  foreignKey: 'product_id'
+});
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
   through: ProductTag,
-  as: product_id,
-  foreignKey: product_id
-})
+  foreignKey: 'tag_id'
+});
 
 module.exports = {
   Product,
